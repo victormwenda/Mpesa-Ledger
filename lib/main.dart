6072118a354a9efe4a520df962e97b4901910c1a
Jama Mohamed <jama3137@gmail.com>
2019-07-03 11:23:04 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(App());
+ 
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Mpesa Ledger'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
