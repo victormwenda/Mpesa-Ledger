611959e3be8724998ab80853f154052c5848d0bf
Jama Mohamed <jama3137@gmail.com>
2019-07-26 16:03:27 +0300

    Finished the totals in summary screen

--- lib/screens/calculator/index.dart
+++ lib/screens/calculator/index.dart
@@ -3,7 +3,6 @@ import 'package:mpesa_ledger_flutter/blocs/calculator/calculator_bloc.dart';
 
 import 'package:mpesa_ledger_flutter/widgets/appbar/appbar.dart';
 import 'package:mpesa_ledger_flutter/widgets/textfields/calculator_textfield.dart';
-import 'package:mpesa_ledger_flutter/widgets/textfields/textfield.dart';
 
 class Calculator extends StatefulWidget {
   CalculatorBloc calculatorBloc = CalculatorBloc();
@@ -16,8 +15,10 @@ class _CalculatorState extends State<Calculator> {
   TextEditingController amount = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
+  void dispose() { 
+    amount.dispose();
+    widget.calculatorBloc.dispose();
+    super.dispose();
   }
 
   @override
@@ -39,169 +40,140 @@ class _CalculatorState extends State<Calculator> {
                 ),
               ),
               Expanded(
-                child: StreamBuilder<Object>(
-                    stream: null,
-                    builder: (context, snapshot) {
-                      return StreamBuilder<Map<String, String>>(
-                          stream: widget.calculatorBloc.transactionFeesStream,
-                          builder: (context, snapshot) {
-                            return ListView(
-                              children: <Widget>[
-                                ListTile(
-                                  title: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Expanded(
-                                        flex: 2,
-                                        child: Text(
-                                          "Send to Mpesa User",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subhead,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Align(
-                                          alignment: Alignment.topRight,
-                                          child: Text(
-                                            snapshot
-                                                .data["transferToMpesaUsers"],
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .title,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
+                  child: StreamBuilder<Map<String, String>>(
+                stream: widget.calculatorBloc.transactionFeesStream,
+                // initialData: ,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView(
+                      children: <Widget>[
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Send to Mpesa User",
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    snapshot.data["transferToMpesaUsers"],
+                                    style: Theme.of(context).textTheme.title,
                                   ),
                                 ),
-                                ListTile(
-                                  title: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Expanded(
-                                        flex: 2,
-                                        child: Text(
-                                          "Send to Other Mobile User",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subhead,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Align(
-                                          alignment: Alignment.topRight,
-                                          child: Text(
-                                            snapshot
-                                                .data["transferToOtherUsers"],
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .title,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Send to Other Mobile User",
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    snapshot.data["transferToOtherUsers"],
+                                    style: Theme.of(context).textTheme.title,
                                   ),
                                 ),
-                                ListTile(
-                                  title: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Expanded(
-                                        flex: 2,
-                                        child: Text(
-                                          "Send to Unregistered Mpesa User",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subhead,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Align(
-                                          alignment: Alignment.topRight,
-                                          child: Text(
-                                            snapshot
-                                                .data["transferToUnregisteredUsers"],
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .title,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Send to Unregistered Mpesa User",
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    snapshot
+                                        .data["transferToUnregisteredUsers"],
+                                    style: Theme.of(context).textTheme.title,
                                   ),
                                 ),
-                                ListTile(
-                                  title: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Expanded(
-                                        flex: 2,
-                                        child: Text(
-                                          "Withdraw From Agent",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subhead,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Align(
-                                          alignment: Alignment.topRight,
-                                          child: Text(
-                                            snapshot
-                                                .data["withdrawFromAgent"],
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .title,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Withdraw From Agent",
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    snapshot.data["withdrawFromAgent"],
+                                    style: Theme.of(context).textTheme.title,
                                   ),
                                 ),
-                                ListTile(
-                                  title: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Expanded(
-                                        flex: 2,
-                                        child: Text(
-                                          "Withdraw From ATM",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subhead,
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Align(
-                                          alignment: Alignment.topRight,
-                                          child: Text(
-                                            snapshot
-                                                .data["atmWithdrawal"],
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .title,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "Withdraw From ATM",
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    snapshot.data["atmWithdrawal"],
+                                    style: Theme.of(context).textTheme.title,
                                   ),
                                 ),
-                              ],
-                            );
-                          });
-                    }),
-              )
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                  return Container();
+                },
+              ))
             ],
           ),
         ),
--- lib/screens/summary/index.dart
+++ lib/screens/summary/index.dart
@@ -4,7 +4,6 @@ import 'package:mpesa_ledger_flutter/blocs/summary/summary_bloc.dart';
 import 'package:mpesa_ledger_flutter/widgets/appbar/appbar.dart';
 
 class Summary extends StatefulWidget {
-
   SummaryBloc summaryBloc = SummaryBloc();
 
   @override
@@ -12,14 +11,102 @@ class Summary extends StatefulWidget {
 }
 
 class _SummaryState extends State<Summary> {
+  bool showHeader = true;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         AppbarWidget("Summary"),
         Expanded(
-            child: Container(
-        )),
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              if (showHeader) {
+                showHeader = false;
+                return StreamBuilder<Map<String, double>>(
+                  stream: widget.summaryBloc.transactionTotalsStream,
+                  initialData: {
+                    "deposits": 0.0,
+                    "withdrawals": 0.0,
+                    "transactionCost": 0.0,
+                  },
+                  builder: (BuildContext context,
+                      AsyncSnapshot<Map<String, double>> snapshot) {
+                    return Container(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Total Deposits",
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(
+                                "KES " + snapshot.data["deposits"].toString(),
+                                style:
+                                    Theme.of(context).textTheme.headline.merge(
+                                          TextStyle(
+                                            color: Color(0XFF4CAF50),
+                                          ),
+                                        ),
+                              ),
+                            ),
+                            Text(
+                              "Total Withdrawals",
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(
+                                "KES " +
+                                    snapshot.data["withdrawals"].toString(),
+                                style:
+                                    Theme.of(context).textTheme.headline.merge(
+                                          TextStyle(
+                                            color: Color(0XFFF44336),
+                                          ),
+                                        ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: RichText(
+                                text: TextSpan(
+                                  text: "KES " +
+                                      snapshot.data["transactionCost"]
+                                          .toString(),
+                                  style:
+                                      Theme.of(context).textTheme.title.merge(
+                                            TextStyle(
+                                              color: Color(0XFFF44336),
+                                            ),
+                                          ),
+                                  children: [
+                                    TextSpan(
+                                      text: "  in transaction costs",
+                                      style:
+                                          Theme.of(context).textTheme.caption,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+              return Text(index.toString());
+            },
+          ),
+        ),
       ],
     );
   }
